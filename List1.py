c58b08493d7e85f71b4b5d76e31f953f1aa3563e
dipankarj571@gmail.com <dipankarj571@gmail.com>
2024-09-14 12:16:21 +0530

    secon commit

--- List1.py
+++ List1.py
@@ -4,20 +4,20 @@
 # Question 4: Remove the first element from the fruits list.
 # Question 5: Find and print the index of the element "grape" in the fruits list.
 
-# def fruit_container():
-#     fruit_names=["apple","banana","orange","grape","mango"]
-#     print(fruit_names[2])    
-#     fruit_names.append("kiwi")
-#     print(fruit_names)
-#     fruit_names.remove("apple")
-#     print(fruit_names)
-#     if 'grape' in fruit_names:
-#         print("grape is founded in fruit list.")
-#         index_of_grape=fruit_names.index("grape")
-#         print("The grape is founded at index no:",index_of_grape)
-#     else:
-#         print("grape is founded in fruit list.")
-# fruit_container()
+def fruit_container():
+    fruit_names=["apple","banana","orange","grape","mango"]
+    print(fruit_names[2])    
+    fruit_names.append("kiwi")
+    print(fruit_names)
+    fruit_names.remove("apple")
+    print(fruit_names)
+    if 'grape' in fruit_names:
+        print("grape is founded in fruit list.")
+        index_of_grape=fruit_names.index("grape")
+        print("The grape is founded at index no:",index_of_grape)
+    else:
+        print("grape is founded in fruit list.")
+fruit_container()
 
 
 
